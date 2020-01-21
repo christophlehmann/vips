d62ca98323f11a714486f01894353a6146012b97
Christoph Lehmann <lehmann@networkteam.com>
2020-01-21 23:53:53 +0100

    Task: Improve performance through thumbnail before crop
    Relates: #1

--- Classes/Resource/Processing/CropScaleMaskHelper.php
+++ Classes/Resource/Processing/CropScaleMaskHelper.php
@@ -4,34 +4,31 @@ namespace Lemming\Vips\Resource\Processing;
 use Jcupitt\Vips\Image;
 use Lemming\Vips\Service\ConfigurationService;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Imaging\GraphicalFunctions;
 use TYPO3\CMS\Core\Log\LogManager;
 use TYPO3\CMS\Core\Resource\Processing\LocalCropScaleMaskHelper;
 use TYPO3\CMS\Core\Resource\Processing\TaskInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\MathUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Frontend\Imaging\GifBuilder;
 
 class CropScaleMaskHelper extends LocalCropScaleMaskHelper
 {
-    protected $configuration = [];
-
-    /**
-     * @inheritDoc
-     */
     public function process(TaskInterface $task)
     {
-        $targetFile = $task->getTargetFile();
-        $fileExtensionIsSupported = GeneralUtility::inList(ConfigurationService::getFileExtensions(),
-            $targetFile->getExtension());
-        $this->configuration = $targetFile->getProcessingConfiguration();
-        $isMaskTask = isset($this->configuration['maskImages']) && is_array($this->configuration['maskImages']);
-
         /** @var $logger \TYPO3\CMS\Core\Log\Logger */
         $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
 
+        $targetFile = $task->getTargetFile();
+        $fileExtensionIsSupported = GeneralUtility::inList(ConfigurationService::getFileExtensions(),
+            $targetFile->getExtension());
+        $configuration = $targetFile->getProcessingConfiguration();
+        $isMaskTask = isset($configuration['maskImages']) && is_array($configuration['maskImages']);
+
         if (!$fileExtensionIsSupported || $isMaskTask) {
             $logger->info(sprintf('Unsupported: %s Fallback to TYPO3', $task->getSourceFile()->getPublicUrl()),
-                $this->configuration);
+                $configuration);
             return parent::process($task);
         }
 
@@ -40,74 +37,117 @@ class CropScaleMaskHelper extends LocalCropScaleMaskHelper
             $targetFileName = $this->getFilenameForImageCropScaleMask($task);
             $originalFileName = $sourceFile->getForLocalProcessing(false);
 
-            if (empty($this->configuration['fileExtension'])) {
-                $this->configuration['fileExtension'] = $task->getTargetFileExtension();
+            if (empty($configuration['fileExtension'])) {
+                $configuration['fileExtension'] = $task->getTargetFileExtension();
             }
 
-            $image = Image::newFromFile($originalFileName);
-            if (!empty($this->configuration['crop'])) {
-                $image = $this->cropImage($image);
-            }
-
-            $image = $this->thumbnail($image, $this->configuration['fileExtension']);
-            $quality = MathUtility::forceIntegerInRange($GLOBALS['TYPO3_CONF_VARS']['GFX']['jpg_quality'], 10, 100, 85);
-            $temporaryFileName = self::getTemporaryFileName($targetFileName);
-            $image->writeToFile($temporaryFileName,
-                [
-                    "Q" => $quality,
-                    "strip" => true
-                ]
+            $gifBuilder = GeneralUtility::makeInstance(GifBuilder::class);
+            $options = $this->getConfigurationForImageCropScaleMask($targetFile, $gifBuilder);
+            $graphicalFunctions = GeneralUtility::makeInstance(GraphicalFunctions::class);
+            $info = $graphicalFunctions->getImageDimensions($originalFileName);
+            $data = $graphicalFunctions->getImageScale(
+                $info,
+                $configuration['width'],
+                $configuration['height'],
+                $options
             );
 
-            $logger->info(sprintf('Succesfully processed %s', $originalFileName), $this->configuration);
+            $cropArea = $this->getCropArea($configuration);
+            if ($cropArea) {
+                $thumbnailFactor = $cropArea[2] / $data[0];
+                $thumbnailWidth = $info[0] / $thumbnailFactor;
+                $thumbnailHeight = $info[1] / $thumbnailFactor;
+                $newCropArea = [
+                    'offsetX' => $cropArea[0] / $thumbnailFactor,
+                    'offsetY' => $cropArea[1] / $thumbnailFactor,
+                    'width' => $data[0],
+                    'height' => $data[1],
+                ];
+
+                $logger->debug('thumbnail croparea ' . $originalFileName, [$thumbnailWidth, $thumbnailHeight]);
+                $image = Image::thumbnail(
+                    $originalFileName,
+                    $thumbnailWidth,
+                    ['height' => $thumbnailHeight]
+                );
+
+                $logger->debug('crop croparea ' . $originalFileName, $newCropArea);
+                $image = $image->crop($newCropArea['offsetX'], $newCropArea['offsetY'], $newCropArea['width'],
+                    $newCropArea['height']);
+            } else {
+                $logger->debug('thumbnail ' . $originalFileName, $data);
+                $image = Image::thumbnail(
+                    $originalFileName,
+                    $data[0],
+                    ['height' => $data[1]]
+                );
+
+                if ($data['crs']) {
+                    if (!$data['origW']) {
+                        $data['origW'] = $data[0];
+                    }
+                    if (!$data['origH']) {
+                        $data['origH'] = $data[1];
+                    }
+                    $offsetX = (int)(($data[0] - $data['origW']) * ($data['cropH'] + 100) / 200);
+                    $offsetY = (int)(($data[1] - $data['origH']) * ($data['cropV'] + 100) / 200);
+
+                    $logger->debug('crop crs ' . $originalFileName, [$offsetX, $offsetY, $data]);
+                    $image = $image->crop($offsetX, $offsetY, $data['origW'], $data['origH']);
+                }
+            }
+
+            $temporaryFileName = self::getTemporaryFileName($targetFileName);
+            $this->saveImage($image, $temporaryFileName);
 
             $result = [
                 'width' => $image->width,
                 'height' => $image->height,
                 'filePath' => $temporaryFileName
             ];
+            $logger->debug('result ' . $originalFileName, $result);
 
             return $result;
         } catch (\Throwable $e) {
             $logger->critical(sprintf('Failed to process %s. Got "%s" in %s:%d', $originalFileName, $e->getMessage(),
-                $e->getFile(), $e->getLine()), $this->configuration);
+                $e->getFile(), $e->getLine()), $configuration);
         }
     }
 
-    protected function cropImage($image)
+    protected function saveImage($image, $temporaryFileName)
     {
-        // check if it is a json object as done in parent
-        $cropData = json_decode($this->configuration['crop']);
-        if ($cropData) {
-            $crop = implode(',', [(int)$cropData->x, (int)$cropData->y, (int)$cropData->width, (int)$cropData->height]);
-        } else {
-            $crop = $this->configuration['crop'];
-        }
-
-        list($offsetLeft, $offsetTop, $newWidth, $newHeight) = explode(',', $crop, 4);
-        $image = $image->crop($offsetLeft, $offsetTop, $newWidth, $newHeight);
-
-        return $image;
-    }
-
-    protected function thumbnail($image, $suffix)
-    {
-        $width = $this->configuration['width'] ?? $this->configuration['maxWidth'] ?? $image->width;
-        $height = $this->configuration['height'] ?? $this->configuration['maxHeight'] ?? $image->height;
-
-        $buffer = $image->writeToBuffer('.' . $this->configuration['fileExtension']);
-        $image = \Jcupitt\Vips\Image::thumbnail_buffer(
-            $buffer,
-            (integer)$width,
+        $quality = MathUtility::forceIntegerInRange($GLOBALS['TYPO3_CONF_VARS']['GFX']['jpg_quality'], 10, 100, 85);
+        $image->writeToFile($temporaryFileName,
             [
-                'height' => $height
+                "Q" => $quality,
+                "strip" => true
             ]
         );
-
-        return $image;
     }
 
-    public static function getTemporaryFileName($fileName) {
+    /**
+     * @param array $configuration
+     * @return array|null [$offsetLeft, $offsetTop, $width, $height]
+     */
+    protected function getCropArea($configuration)
+    {
+        if (!empty($configuration['crop'])) {
+            // check if it is a json object as done in parent
+            $cropArea = json_decode($configuration['crop']);
+            if ($cropArea) {
+                $crop = implode(',',
+                    [(int)$cropArea->x, (int)$cropArea->y, (int)$cropArea->width, (int)$cropArea->height]);
+            } else {
+                $crop = $configuration['crop'];
+            }
+            $cropArea = explode(',', $crop, 4);
+
+            return $cropArea;
+        }
+    }
+
+    public static function getTemporaryFileName($fileName)
+    {
         if (version_compare(VersionNumberUtility::getNumericTypo3Version(), 9, '<')) {
             $temporaryFileName = PATH_site . 'typo3temp/' . $fileName;
         } else {
