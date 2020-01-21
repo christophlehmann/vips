db6d408a5bed544341a56e4493de545beaa765c4
Christoph Lehmann <lehmann@networkteam.com>
2020-01-21 23:43:59 +0100

    Task: Show error in report if vips version is below 8.8

--- Classes/Report/Report.php
+++ Classes/Report/Report.php
@@ -11,12 +11,13 @@ class Report implements StatusProviderInterface
     public function getStatus()
     {
         $isLoaded = extension_loaded('vips');
+        $versionSupported = $isLoaded && version_compare(vips_version(), '8.8', '>=');
         $status['vips'] = GeneralUtility::makeInstance(ObjectManager::class)->get(
             Status::class,
             'PHP Module vips',
             $isLoaded ? 'Version ' . vips_version() : 'Not loaded',
             '',
-            $isLoaded ? Status::OK : Status::ERROR
+            $isLoaded && $versionSupported ? Status::OK : Status::ERROR
         );
 
         return $status;
